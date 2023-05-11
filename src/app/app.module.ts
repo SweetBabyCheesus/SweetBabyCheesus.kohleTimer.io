9ec8e6825db6e3f7f219588bb145d57dda3b31f2
Jonas Stark <jonas.stark@freenet.de>
2023-05-11 12:49:53 +0200

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ClipboardModule } from '@angular/cdk/clipboard';
 import { AppComponent } from './app.component';
 import { TimePickerComponent } from './time-picker/time-picker.component';
 
 @NgModule({
   declarations: [AppComponent, TimePickerComponent],
-  imports: [BrowserModule, NgxMaterialTimepickerModule.setLocale("de-DE"), BrowserAnimationsModule ],
+  imports: [
+    BrowserModule,
+    NgxMaterialTimepickerModule.setLocale('de-DE'),
+    BrowserAnimationsModule,
+    ClipboardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
