991e8ef014849fd0e9a0b65f322f41b861cb4f34
Jonas Stark <jonas.stark@freenet.de>
2023-05-11 21:49:14 +0200

    img

--- src/shared/utils/notyf.token.ts
+++ src/shared/utils/notyf.token.ts
@@ -5,7 +5,7 @@ export const NOTYF = new InjectionToken<Notyf>('NotyfToken');
 
 export function notyfFactory(): Notyf {
   return new Notyf({
-      duration: 50000,
+      duration: 5000,
       position: {
         x: 'right',
         y: 'top',
@@ -13,7 +13,7 @@ export function notyfFactory(): Notyf {
       types: [
         {
           type: 'success',
-          background: 'url(https://raw.githubusercontent.com/SweetBabyCheesus/SweetBabyCheesus.kohleTimer.io/9dab1c7e5e041f4046f0a95c564b4ed026dfb200/assets/images/c.jpg)',
+          
         },
       ]
     });
